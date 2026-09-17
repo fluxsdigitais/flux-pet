61d1da07dc9cf3277ca3e0a509b8432d889f4e15
Leo Dev Flux <leodevflux@gmail.com>
2026-09-17 17:23:05 -0300

    Configure project for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  output: "standalone",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
